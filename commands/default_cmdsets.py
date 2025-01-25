6b022d4ecc9de02f6ea427ed09d4923fcdc671ac
daiimus <daiimus@gmail.com>
2025-01-25 15:19:22 -0800

    Refactor CmdAttack reference in CharacterCmdSet to use CmdCombat namespace

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -34,7 +34,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
-        self.add(CmdAttack)
+        self.add(CmdCombat.CmdAttack)
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
     """
