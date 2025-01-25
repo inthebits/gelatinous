192f5428d324c39954a179de72d3422b32d18f08
daiimus <daiimus@gmail.com>
2025-01-25 15:03:17 -0800

    Testing Yet again

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -34,7 +34,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
-        self.add(CmdCombat.CmdAttack)
+        self.add(CmdAttack)
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
     """
