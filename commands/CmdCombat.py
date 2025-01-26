71ae283b299efeda26cd10cc950b43e22598689d
daiimus <daiimus@gmail.com>
2025-01-25 20:42:19 -0800

    added character

--- commands/CmdCombat.py
+++ commands/CmdCombat.py
@@ -16,6 +16,6 @@ class CmdAttack(Command):
 
     def func(self):
         self.caller.msg(f"Echo: '{self.args.strip()}'")
-        grit = self.db.value('grit', category='stat')
+        grit = character.db.value('grit', category='stat')
         self.caller.msg(f"Your grit is: {grit}.'")
 # ...
