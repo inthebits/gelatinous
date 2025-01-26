ee0cd6bbe730f67584bbdb5666f03ef6dcc264ff
daiimus <daiimus@gmail.com>
2025-01-25 19:07:16 -0800

    singular

--- commands/CmdCombat.py
+++ commands/CmdCombat.py
@@ -16,6 +16,6 @@ class CmdAttack(Command):
 
     def func(self):
         self.caller.msg(f"Echo: '{self.args.strip()}'")
-        grit = self.attributes.get('grit', category='stat')
+        grit = self.attribute.get('grit', category='stat')
         self.caller.msg(f"Your grit is: {grit}.'")
 # ...
