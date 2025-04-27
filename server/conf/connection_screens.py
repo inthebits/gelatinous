e047f9be0136ba311006dfe4a5389cd1ca571c0b
daiimus <daiimus@gmail.com>
2025-04-27 10:32:57 -0700

    Default

--- server/conf/connection_screens.py
+++ server/conf/connection_screens.py
@@ -1,53 +1,40 @@
+# -*- coding: utf-8 -*-
 """
-Connection screen file for Evennia.
-This version randomizes signal number, glitch message, and year.
-"""
+Connection screen
 
-import random
+This is the text to show the user when they first connect to the game (before
+they log in).
 
-def get_connection_screen():
-    """
-    Dynamically generates a connection screen with slight randomization
-    for atmosphere (signal degradation, glitches).
-    """
-    # Random 2-digit signal number
-    signal_number = f"{random.randint(0, 99):02d}"
+To change the login screen in this module, do one of the following:
 
-    # Random glitch message
-    glitch_messages = [
-        "[ WARNING: Signal instability detected. ]",
-        "[ ERROR: Calibration pattern failed. ]",
-        "[ ALERT: Tone sequence corrupted. ]",
-        "[ NOTICE: Broadcast medium deteriorated. ]"
-    ]
-    glitch_message = random.choice(glitch_messages)
+- Define a function `connection_screen()`, taking no arguments. This will be
+  called first and must return the full string to act as the connection screen.
+  This can be used to produce more dynamic screens.
+- Alternatively, define a string variable in the outermost scope of this module
+  with the connection string that should be displayed. If more than one such
+  variable is given, Evennia will pick one of them at random.
 
-    # Random corrupted year
-    year_display = random.choice(["198█", "NULL"])
+The commands available to the user when the connection screen is shown
+are defined in evennia.default_cmds.UnloggedinCmdSet. The parsing and display
+of the screen is done by the unlogged-in "look" command.
 
-    # Return the full screen
-    return fr"""
-█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-█▒▒▒ GELATINOUS MONSTER SYSTEM :: SIGNAL {signal_number} ▒▒▒▒█
-█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
-
-{glitch_message}
-[ Color bars desaturated. ]
-[ Anomalous resonance detected at 7.8Hz. ]
-
-YEAR: {year_display} (ENDLESS BROADCAST)
-LOCATION: PARTS UNKNOWN
-
->> Streets: Flowing.
->> Airwaves: Distorted.
->> Flesh: Grainy.
->> Memory: OFFLINE.
-
-__ Connect : connect <accountname> <password>
-__ Create  : create <accountname> <password>
-
->>> END OF TEST PATTERN. BROADCAST WILL NOT RESUME.
-
-█████████████████████████████████████████████████████
 """
 
+from django.conf import settings
+
+from evennia import utils
+
+CONNECTION_SCREEN = """
+|b==============================================================|n
+ Welcome to |g{}|n, version {}!
+
+ If you have an existing account, connect to it by typing:
+      |wconnect <username> <password>|n
+ If you need to create an account, type (without the <>'s):
+      |wcreate <username> <password>|n
+
+ If you have spaces in your username, enclose it in quotes.
+ Enter |whelp|n for more info. |wlook|n will re-show this screen.
+|b==============================================================|n""".format(
+    settings.SERVERNAME, utils.get_evennia_version("short")
+)
