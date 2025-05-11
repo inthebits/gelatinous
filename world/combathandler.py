7979a681ba002c0acd31446b1a35271bf7ff0082
daiimus <daiimus@gmail.com>
2025-05-10 18:16:27 -0700

    Adjust self.stop

--- world/combathandler.py
+++ world/combathandler.py
@@ -72,7 +72,6 @@ class CombatHandler(DefaultScript):
             if char.ndb.combat_handler:
                 del char.ndb.combat_handler
         splattercast.msg("Combat ends.")
-        self.stop()
         self.is_active = False
         self.delete()
 
